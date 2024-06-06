893d6e34aba619aa4cc937b9886b4bb5a6a1bd8b
sreenikithareddy <sreenikithareddy@gmail.com>
2024-06-06 06:15:29 +0530

    assignment2

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,17 +4,17 @@ import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses"; 
 import { Link } from 'react-router-dom';
+import "./styles.css";
 
 export default function Kanbas() {
   return (
-    <div id="wd-kanbas">
-      <table>
-        <tbody>
-          <tr>
-            <td valign="top">
-              <KanbasNavigation />
-            </td>
-            <td valign="top">
+    <div id="wd-kanbas" className="h-100">
+      <div className="d-flex h-100">
+    <div className="d-none d-md-block bg-black">
+      <KanbasNavigation />
+    </div>
+            
+            <div className="flex-fill p-4">
               <Routes>
                 <Route path="/" element={<Navigate to="Dashboard" />} />
                 <Route path="Account" element={<h1>Account</h1>} />
@@ -23,11 +23,10 @@ export default function Kanbas() {
                 <Route path="Calendar" element={<h1>Calendar</h1>} />
                 <Route path="Inbox" element={<h1>Inbox</h1>} />
               </Routes>
+              </div>
+              </div>
               <Link to="/LandingPage">Back to Landing Page</Link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
     </div>
+  
   );
 }
