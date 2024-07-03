ce5401e336a7000e15b6c9d62a0e770d52388a84
sreenikithareddy <sreenikithareddy@gmail.com>
2024-07-04 01:40:30 +0530

    A4

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,13 @@ import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
 import LandingPage from './LandingPage';
+import store from './Labs/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
     <HashRouter>
+       <Provider store={store}>
       <div>
         <Routes>
         <Route path="/" element={<Navigate to="LandingPage" />} />
@@ -31,6 +34,7 @@ function App() {
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
+      </Provider>
     </HashRouter>
   );
 }
